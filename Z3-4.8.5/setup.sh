ec622278b62a8edca88fc2381e6a1ee3265260fa
humeafo <humeafo@gmail.com>
2019-07-24 16:28:31 +0800

    added cvc4 1.7 and z3 4.8.5 after smtcomp 2019

---
+++ Z3-4.8.5/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version=4.8.5
+branch=Z3-4.8.5
+
+cmake_files_dir=$base_dir/Z3-git
+
+source $base_dir/Z3-git/shared.sh
---
+++ cvc4-1.7/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+version=1.7
+
+config_files_dir=$base_dir/cvc4-git
+
+source $base_dir/cvc4-git/shared.sh
