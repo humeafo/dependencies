7f881b040e549c608ce0a815fbb1f6c4409bdd85
humeafo <humeafo@gmail.com>
2019-07-24 17:46:00 +0800

    fix merge mistake

--- yices-git/setup.sh
+++ yices-git/setup.sh
@@ -3,10 +3,6 @@
 version=git
 branch=master
 
-<<<<<<< HEAD
-cmake_files_dir=$base_dir/yices-git
-=======
 config_files_dir=$base_dir/yices-git
->>>>>>> 4e6934874d557da8fd7fe56310c25adcc5bb35ba
 
 source $base_dir/yices-git/shared.sh
