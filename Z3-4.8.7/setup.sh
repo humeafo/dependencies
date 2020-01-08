b13b5bfbd63bc94cf832d2d8b49b74b161e41fce
humeafo <humeafo@gmail.com>
2020-01-08 16:34:48 +0800

    upgrade to support z3 boolector and boost version

---
+++ Z3-4.8.7/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version=4.8.7
+branch=z3-4.8.7
+
+cmake_files_dir=$base_dir/Z3-4.8.7
+
+source $base_dir/Z3-4.8.7/shared.sh
---
+++ boost-1_72_0-fs/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+version=1_72_0
+version_=1.72.0
+
+source "$base_dir/boost-${version}-fs/shared.sh"
+
+# vim: ts=2 sw=2 et
